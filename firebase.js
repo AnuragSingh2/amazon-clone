4e7e4cef7e9e38bff4edabb0355dbd572b9f9df3
Anurag Singh <singh.anurag1905@gmail.com>
2021-05-22 17:17:02 +0530

    build checkout page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5kcNJ44L4oq7lKXHlDegy4lKZ2Vis7yw",
+  authDomain: "clone-538bf.firebaseapp.com",
+  projectId: "clone-538bf",
+  storageBucket: "clone-538bf.appspot.com",
+  messagingSenderId: "665279139438",
+  appId: "1:665279139438:web:3ea7a3ea07a868a1b4a07f",
+};
